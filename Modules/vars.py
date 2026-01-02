8ced2004d03b127243b0f4fbc7b7db25a9253202
bandale6162-cmyk <bandale.6162@gmail.com>
2026-01-02 20:16:14 +0530

    Update API credentials and auth user IDs

--- Modules/vars.py
+++ Modules/vars.py
@@ -2,12 +2,13 @@
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
-API_ID    = os.environ.get("API_ID", "10868873")
-API_HASH  = os.environ.get("API_HASH", "2ce261d1e624c9874548f5498266a336")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+API_ID    = os.environ.get("API_ID", "20527556")
+API_HASH  = os.environ.get("API_HASH", "85d84f730c4e2f13bdf3efbce7d3352e")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7735182633:AAGtainmSXNe LHnkbXU7zXcmiEwd78QXq-s") 
 LOG = True  
-auth_user = os.environ.get('auth_users', '7602994049').split(',')
-auth_users = [int(user_id) for user_id in auth_user]
+auth_user = os.environ.get('auth_users', '8349653190').split(',')
+auth_users = [int(Nature) for Nature in 8349653190]
 
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
